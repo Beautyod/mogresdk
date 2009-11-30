c5c0407d8bfd3a59fae08bcf7cb6479ca699bb54
beautyod <beautyod@gmx.de>
2009-11-30 02:18:34 +0000

    added forgotten file

---
+++ sdk_fs/BuildSamplesX64.cmd
@@ -0,0 +1,6 @@
+%windir%\Microsoft.NET\Framework\v3.5\MSBuild /property:Configuration=Release /property:Platform=x64 "MogreSamples.sln"
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@exit /B 0
+:err
+@PAUSE
+@exit /B 1
