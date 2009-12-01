513091d3594aefa3f1e36c1f24f50f652011833a
GKAgent022 <GKAgent022@64d068d0-dcfc-11de-a566-113fb6a08965>
2009-12-01 22:51:09 +0000

    Implemented a configuration serializer for reading and writing the configuration of the sample browser.

---
+++ demo_starter/source/Mogre.SDK.SampleBrowser/ConfigurationSerializer.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Mogre.SDK.SampleBrowser
+{
+    public class ConfigurationSerializer
+    {
+        private readonly XmlSerializer _serializer;
+
+        public ConfigurationSerializer()
+        {
+            _serializer = new XmlSerializer(typeof (SampleBrowser));
+        }
+
+        public void Serialize(string filename, Sample[] samples)
+        {
+            var sampleBrowser = new SampleBrowser {Samples = samples};
+
+            using (var stream = new FileStream(filename, FileMode.Create))
+                _serializer.Serialize(stream, sampleBrowser);
+        }
+
+        public Sample[] Deserialize(string filename)
+        {
+            SampleBrowser sampleBrowser;
+
+            try
+            {
+                using (var stream = new FileStream(filename, FileMode.Open))
+                    sampleBrowser = (SampleBrowser) _serializer.Deserialize(stream);
+            }
+            catch
+            {
+                return null;
+            }
+
+            return sampleBrowser.Samples;
+        }
+
+        [XmlRoot("sampleBrowser")]
+        public struct SampleBrowser
+        {
+            [XmlElement("sample")] 
+            public Sample[] Samples { get; set; }
+        }
+
+        public struct Sample
+        {
+            [XmlElement("name")]
+            public string Name { get; set; }
+
+            [XmlElement("description")]
+            public string Description { get; set; }
+
+            [XmlElement("category")]
+            public string Category { get; set; }
+
+            [XmlElement("executablePath")]
+            public string ExecutablePath { get; set; }
+
+            [XmlElement("previewImagePath")]
+            public string PreviewImagePath { get; set; }
+        }
+    }
+}
