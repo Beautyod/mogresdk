edb3a73ab36cef5faa4a5156004a431e56dae4ed
GKAgent022 <GKAgent022@64d068d0-dcfc-11de-a566-113fb6a08965>
2010-01-05 16:23:59 +0000

    - Done a workaround for the fclose bug in tlibc. For details, see dependency_checkers/checker_kwertz_cpp/source/application.cpp.

--- trunk/dependency_checkers/checker_kwertz_cpp/source/application.cpp
+++ trunk/dependency_checkers/checker_kwertz_cpp/source/application.cpp
@@ -1,5 +1,6 @@
 #define ANFP
 #define _CRT_SECURE_NO_WARNINGS
+#define FCLOSE_WORKAROUND
 
 #include "stdinc.h"
 
@@ -12,7 +13,12 @@ bool FileExists(const char* filename)
 
 	file = fopen(filename, "r");
 	bool exists = !(file == NULL);
+
+	// BUG: On some systems, the closing a file using tlibc causes an access violation
+	// WORKAROUND: Don't do it, simply!
+#ifndef FCLOSE_WORKAROUND
 	fclose(file);
+#endif
 
 	return exists;
 }
