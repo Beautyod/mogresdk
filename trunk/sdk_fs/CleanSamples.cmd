51cbc82dbf06883f06355db92e813321ddb01c86
GKAgent022 <GKAgent022@64d068d0-dcfc-11de-a566-113fb6a08965>
2010-01-14 17:06:54 +0000

    - Fixed the CleanSamples.cmd script

--- trunk/sdk_fs/CleanSamples.cmd
+++ trunk/sdk_fs/CleanSamples.cmd
@@ -1,4 +1,6 @@
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild /target:Clean "MogreSamples.sln"
+%windir%\Microsoft.NET\Framework\v3.5\MSBuild /property:Configuration=Release /target:Clean "MogreSamples.sln"
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+%windir%\Microsoft.NET\Framework\v3.5\MSBuild /property:Configuration=Debug /target:Clean "MogreSamples.sln"
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 :err
