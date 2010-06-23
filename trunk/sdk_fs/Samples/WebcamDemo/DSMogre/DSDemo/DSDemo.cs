3a70f7d80a7bcb9d7b462b784931c8ba11477083
beautyod@gmx.de <beautyod@gmx.de@64d068d0-dcfc-11de-a566-113fb6a08965>
2010-06-23 08:23:15 +0000

    added WebcamDemo (still not working in the SDK environment)

---
+++ trunk/sdk_fs/Samples/WebcamDemo/DSMogre/DSDemo/DSDemo.cs
@@ -0,0 +1,183 @@
+namespace DSDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Drawing;
+
+    using DSMogre;
+
+    using Mogre;
+
+    public class DSDemoApp
+    {
+        #region Fields
+
+        private Camera camera;
+        private int captureSource;
+        private CaptureToTexture ctt;
+        private Root root;
+        private SceneManager sceneMgr;
+        private Viewport viewport;
+        private RenderWindow window;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public DSDemoApp(int captureSource)
+        {
+            this.captureSource = captureSource - 2;
+            this.Setup();
+
+            this.root.StartRendering();
+            this.ctt.Dispose();
+
+            this.root.Dispose();
+            this.root = null;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        #region Private Static Methods
+
+        private static int NextPowerOfTwo(int val)
+        {
+            double vald = val;
+            return (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(vald, 2)));
+        }
+
+        #endregion Private Static Methods
+
+        #region Private Methods
+
+        private bool Scene_FrameStarted(FrameEvent evt)
+        {
+            if (this.window.IsClosed)
+            {
+                return false;
+            }
+
+            // update the video texture
+            this.ctt.Update();
+
+            return true;
+        }
+
+        private void Setup()
+        {
+            this.root = new Root();
+            this.SetupResources();
+
+            this.root.RestoreConfig();
+            this.window = this.root.Initialise(true);
+
+            // Get the SceneManager, in this case a generic one
+            this.sceneMgr = Root.Singleton.CreateSceneManager("DefaultSceneManager");
+
+            // Load resources
+            ResourceGroupManager.Singleton.InitialiseAllResourceGroups();
+
+            this.root.FrameStarted += new FrameListener.FrameStartedHandler(this.Scene_FrameStarted);
+
+            // Create the camera
+            this.camera = this.sceneMgr.CreateCamera("PlayerCam");
+
+            this.camera.NearClipDistance = 5;
+            this.camera.FarClipDistance = 0;
+            this.camera.FOVy = 45;
+
+            this.viewport = this.window.AddViewport(this.camera);
+            this.viewport.BackgroundColour = ColourValue.White;
+
+            this.camera.Position = new Vector3 (100, 100, 100);
+
+            this.camera.LookAt(new Vector3 (0, 0, 0));
+            Light l = sceneMgr.CreateLight();
+            l.DiffuseColour = ColourValue.White;
+            l.Position = new Vector3(450, 200, 1000);
+            l.Type = Light.LightTypes.LT_POINT;
+
+            l.PowerScale = 20;
+
+            this.SetupCapture();
+        }
+
+        private void SetupCapture()
+        {
+            Capture cap;
+            if (this.captureSource == -1)
+            {
+                cap = new VideoFileCapture("test.avi")
+                {
+                    Looping = true
+                };
+            }
+            else
+            {
+                cap = new WebcamCapture(this.captureSource);
+            }
+
+            this.ctt = new CaptureToTexture(cap, "vidtex");
+
+            // get video size
+            this.ctt.Start();
+            while (!cap.IsRunning)
+            {
+            }
+            this.ctt.Stop();
+            Size s = new Size(NextPowerOfTwo(cap.Size.Width), NextPowerOfTwo(cap.Size.Height));
+
+            using (TexturePtr tex = TextureManager.Singleton.CreateManual(
+                "vidtex",
+                ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME,
+                TextureType.TEX_TYPE_2D,
+                (uint)s.Width,
+                (uint)s.Height,
+                0,
+                PixelFormat.PF_A8R8G8B8))
+            {
+                using (MaterialPtr mat = MaterialManager.Singleton.Create("vidmat", ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME))
+                {
+                    mat.GetTechnique(0).CreatePass().CreateTextureUnitState(tex.Name);
+                    Entity e = this.sceneMgr.CreateEntity("cube", "cube.mesh");
+                    e.SetMaterial(mat);
+                    this.sceneMgr.RootSceneNode.CreateChildSceneNode().AttachObject(e);
+                }
+            }
+
+            this.ctt.Start();
+        }
+
+        private void SetupResources()
+        {
+            // Load resource paths from config file
+            ConfigFile cf = new ConfigFile();
+            cf.Load("resources.cfg", "\t:=", true);
+
+            // Go through all sections & settings in the file
+            ConfigFile.SectionIterator seci = cf.GetSectionIterator();
+
+            string secName, typeName, archName;
+
+            // Normally we would use the foreach syntax, which enumerates the values, but in this case we need CurrentKey too;
+            while (seci.MoveNext())
+            {
+                secName = seci.CurrentKey;
+                ConfigFile.SettingsMultiMap settings = seci.Current;
+                foreach (KeyValuePair<string, string> pair in settings)
+                {
+                    typeName = pair.Key;
+                    archName = pair.Value;
+                    ResourceGroupManager.Singleton.AddResourceLocation(archName, typeName);
+                }
+            }
+        }
+
+        #endregion Private Methods
+
+        #endregion Methods
+    }
+}
---
+++ trunk/sdk_fs/Samples/WebcamDemo/DSMogre/DSMogre/WebcamCapture.cs
@@ -0,0 +1,182 @@
+﻿namespace DSMogre
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    using DirectShowLib;
+
+    public sealed class WebcamCapture : Capture
+    {
+        #region Fields
+
+        private readonly int deviceNum;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public WebcamCapture()
+            : this(0)
+        {
+        }
+
+        public WebcamCapture(int deviceNum)
+        {
+            this.deviceNum = deviceNum;
+        }
+
+        public WebcamCapture(string deviceName)
+        {
+            if (Capture.CaptureDeviceNames.Contains(deviceName))
+            {
+                this.deviceNum = Capture.CaptureDeviceNames.IndexOf(deviceName);
+            }
+            else
+            {
+                throw new ArgumentException("Not a valid device name", "deviceName");
+            }
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        #region Protected Methods
+
+        protected override void SetupGraph()
+        {
+            DsDevice[] capDevices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+            if (this.deviceNum + 1 > capDevices.Length)
+            {
+                throw new Exception("No video capture devices found at that index!");
+            }
+
+            DsDevice dev = capDevices[this.deviceNum];
+
+            this.HasBeenSetup = true;
+            IBaseFilter capFilter = null;
+            ICaptureGraphBuilder2 capGraph = null;
+
+            // Get the graphbuilder object
+            this.FilterGraph = (IFilterGraph2)new FilterGraph();
+            this.MediaCtrl = this.FilterGraph as IMediaControl;
+
+            try
+            {
+                // Get the ICaptureGraphBuilder2
+                capGraph = (ICaptureGraphBuilder2) new CaptureGraphBuilder2();
+
+                // Get the SampleGrabber interface
+                this.SampGrabber = (ISampleGrabber) new SampleGrabber();
+
+                // Start building the graph
+                int hr = capGraph.SetFiltergraph(this.FilterGraph);
+                DsError.ThrowExceptionForHR(hr);
+
+                // Add the video device
+                hr = this.FilterGraph.AddSourceFilterForMoniker(dev.Mon, null, "Video input", out capFilter);
+                DsError.ThrowExceptionForHR(hr);
+
+                IBaseFilter baseGrabFlt = (IBaseFilter)this.SampGrabber;
+                this.ConfigureSampleGrabber(this.SampGrabber);
+
+                // Add the frame grabber to the graph
+                hr = this.FilterGraph.AddFilter(baseGrabFlt, "Ds.NET Grabber");
+                DsError.ThrowExceptionForHR(hr);
+
+                this.MediaEvent = this.FilterGraph as IMediaEvent;
+
+                this.SampGrabber.SetBufferSamples(true);
+                this.SampGrabber.SetOneShot(false);
+
+                // If any of the default config items are set
+                if (this.FrameRate + this.Size.Height + this.Size.Width > 0)
+                {
+                    this.SetConfigParms(capGraph, capFilter);
+                }
+
+                hr = capGraph.RenderStream(PinCategory.Capture, MediaType.Video, capFilter, null, baseGrabFlt);
+                DsError.ThrowExceptionForHR(hr);
+
+                this.SaveSizeInfo(this.SampGrabber);
+            }
+            finally
+            {
+                if (capFilter != null)
+                {
+                    Marshal.ReleaseComObject(capFilter);
+                }
+
+                if (this.SampGrabber != null)
+                {
+                    Marshal.ReleaseComObject(this.SampGrabber);
+                    this.SampGrabber = null;
+                }
+
+                if (capGraph != null)
+                {
+                    Marshal.ReleaseComObject(capGraph);
+                }
+            }
+        }
+
+        #endregion Protected Methods
+
+        #region Private Methods
+
+        private void SetConfigParms(ICaptureGraphBuilder2 capGraph, IBaseFilter capFilter)
+        {
+            object o;
+            AMMediaType media;
+
+            // Find the stream config interface
+            capGraph.FindInterface(PinCategory.Capture, MediaType.Video, capFilter, typeof(IAMStreamConfig).GUID, out o);
+
+            IAMStreamConfig videoStreamConfig = o as IAMStreamConfig;
+            if (videoStreamConfig == null)
+            {
+                throw new Exception("Failed to get IAMStreamConfig");
+            }
+
+            // Get the existing format block
+            int hr = videoStreamConfig.GetFormat(out media);
+            DsError.ThrowExceptionForHR(hr);
+
+            // copy out the videoinfoheader
+            VideoInfoHeader v = new VideoInfoHeader();
+            Marshal.PtrToStructure(media.formatPtr, v);
+
+            // if overriding the framerate, set the frame rate
+            if (this.FrameRate > 0)
+            {
+                v.AvgTimePerFrame = 10000000 / this.FrameRate;
+            }
+
+            // if overriding the width, set the width
+            if (this.Size.Width > 0)
+            {
+                v.BmiHeader.Width = this.Size.Width;
+            }
+
+            // if overriding the Height, set the Height
+            if (this.Size.Height > 0)
+            {
+                v.BmiHeader.Height = this.Size.Height;
+            }
+
+            // Copy the media structure back
+            Marshal.StructureToPtr(v, media.formatPtr, false);
+
+            // Set the new format
+            hr = videoStreamConfig.SetFormat(media);
+            DsError.ThrowExceptionForHR(hr);
+
+            DsUtils.FreeAMMediaType(media);
+        }
+
+        #endregion Private Methods
+
+        #endregion Methods
+    }
+}
